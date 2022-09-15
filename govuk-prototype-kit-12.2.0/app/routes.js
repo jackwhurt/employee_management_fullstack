a7d9e0e28a4a13972e04b9a04da5292d54a821f6
mei chen <mei.chen@kainos.com>
2022-09-15 11:48:02 +0100

    added employee routing

--- govuk-prototype-kit-12.2.0/app/routes.js
+++ govuk-prototype-kit-12.2.0/app/routes.js
@@ -1,6 +1,27 @@
 const express = require('express')
 const router = express.Router()
 
+const employeeData = require('./db/employeeData');
+
 // Add your routes here - above the module.exports line
+
+router.get('/add-employee-form', async (req, res) => {
+
+  res.render('add-employee-form')
+});
+
+router.post('/add-employee', async (req, res) => {
+
+  var employee = req.body;
+
+  let result = await employeeData.addEmployee( employee );
+
+  if ( !result ) {
+    res.locals.errormessage = "please check your fields"
+    res.render('add-employee-form', req.body)
+  }
+});
+
+
 
 module.exports = router
