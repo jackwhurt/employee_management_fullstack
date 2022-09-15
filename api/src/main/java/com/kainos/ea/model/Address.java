f66b8b050555d7e77c21e06cbb539eb926271fc0
Jack Hurt <jackwhurt@gmail.com>
2022-09-15 10:13:43 +0100

    actually added files

--- api/src/main/java/com/kainos/ea/model/Address.java
+++ api/src/main/java/com/kainos/ea/model/Address.java
@@ -1,2 +1,74 @@
-package com.kainos.ea.model;public class Addresses {
+package com.kainos.ea.model;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.checkerframework.checker.signature.qual.Identifier;
+import org.checkerframework.common.aliasing.qual.Unique;
+
+import javax.validation.constraints.NotNull;
+
+public class Address {
+
+    @NotNull
+    @Identifier
+    @Unique
+    private int AddressID;
+    @Unique
+    @NotNull
+    private String AddressLineOne;
+    private String Town;
+    private String County;
+    @NotNull
+    private String Postcode;
+
+    @JsonCreator
+    public Address(@JsonProperty("AddressID") @Identifier @Unique int addressID,
+                   @JsonProperty("AddressLineOne") @Unique String addressLineOne,@JsonProperty("Town") String town,
+                   @JsonProperty("County") String county,@JsonProperty("Postcode")String postcode) {
+        AddressID = addressID;
+        AddressLineOne = addressLineOne;
+        Town = town;
+        County = county;
+        Postcode = postcode;
+    }
+
+    public int getAddressID() {
+        return AddressID;
+    }
+
+    public void setAddressID(int addressID) {
+        AddressID = addressID;
+    }
+
+    public String getAddressLineOne() {
+        return AddressLineOne;
+    }
+
+    public void setAddressLineOne(String addressLineOne) {
+        AddressLineOne = addressLineOne;
+    }
+
+    public String getTown() {
+        return Town;
+    }
+
+    public void setTown(String town) {
+        Town = town;
+    }
+
+    public String getCounty() {
+        return County;
+    }
+
+    public void setCounty(String county) {
+        County = county;
+    }
+
+    public String getPostcode() {
+        return Postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        Postcode = postcode;
+    }
 }
--- api/src/main/java/com/kainos/ea/model/SalesEmployee.java
+++ api/src/main/java/com/kainos/ea/model/SalesEmployee.java
@@ -1,2 +1,48 @@
-package com.kainos.ea.model;public class SalesEmployee {
+package com.kainos.ea.model;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.checkerframework.checker.signature.qual.Identifier;
+
+import javax.validation.constraints.NotNull;
+
+public class SalesEmployee {
+
+    @NotNull
+    @Identifier
+    private int EmployeeID;
+    private float Commision;
+    private float TotalSales;
+
+    @JsonCreator
+    public SalesEmployee(@JsonProperty("EmployeeID") @Identifier int employeeID,
+                         @JsonProperty("Commision") float commision,@JsonProperty("TotalSales") float totalSales) {
+        EmployeeID = employeeID;
+        Commision = commision;
+        TotalSales = totalSales;
+    }
+
+    public int getEmployeeID() {
+        return EmployeeID;
+    }
+
+    public void setEmployeeID(int employeeID) {
+        EmployeeID = employeeID;
+    }
+
+    public float getCommision() {
+        return Commision;
+    }
+
+    public void setCommision(float commision) {
+        Commision = commision;
+    }
+
+    public float getTotalSales() {
+        return TotalSales;
+    }
+
+    public void setTotalSales(float totalSales) {
+        TotalSales = totalSales;
+    }
 }
